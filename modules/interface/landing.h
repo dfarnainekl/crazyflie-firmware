5e6c7b2dfb3e8b1a518063bf48a57a69aff848c2
Franky333 <1.daniel.frank@gmail.com>
2015-08-15 19:06:19 +0200

    added takeoff files (should have beed in last commit)

---
+++ modules/interface/landing.h
@@ -0,0 +1,23 @@
+/*
+ * landing.h
+ *
+ *  Created on: 15.08.2015
+ *      Author: Franky333
+ */
+
+#ifndef MODULES_INTERFACE_LANDING_H_
+#define MODULES_INTERFACE_LANDING_H_
+
+#include <stdint.h>
+
+#define LANDING_THRUST 30000
+#define LANDING_YAWRATE 0.0
+#define LANDING_PITCH 0.0
+#define LANDING_ROLL 0.0
+#define LANDING_DURATION 2.0 //time in s for slow descent, after which motors get turned off
+#define LANDING_DELAY 1.0 //time in s for falling, after which landing is finished
+
+void landing_update();
+void landing_getRPYT(float *roll, float *pitch, float *yawRate, uint16_t *thrust);
+
+#endif /* MODULES_INTERFACE_LANDING_H_ */
