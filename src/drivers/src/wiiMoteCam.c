26d9be9fef02164e2c054940df1303db49af6e4a
Franky333 <1.daniel.frank@gmail.com>
2016-05-13 17:15:08 +0200

    added files

---
+++ src/drivers/src/wiiMoteCam.c
@@ -0,0 +1,100 @@
+/*
+ * wiiMoteCam.c
+ *
+ *  Created on: Feb 22, 2015
+ *      Author: Franky333
+ */
+
+#define DEBUG_MODULE "WIIMOTECAM"
+
+#include "stm32fxxx.h"
+// TA: Maybe not so good to bring in these dependencies...
+#include "debug.h"
+#include "eprintf.h"
+#include "i2cdev.h"
+
+#include "wiiMoteCam.h"
+
+//initializes the wmc with basic (unknown) settings
+uint8_t wmc_init_basic() //FIXME: if wmc not connected, bus gets blocked (scl low) --> no eeprom comm
+{
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x30, 0x01)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x30, 0x08)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x06, 0x90)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x08, 0xC0)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x1A, 0x40)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x33, 0x33)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+
+	DEBUG_PRINT("I2C connection [OK].\n");
+	return 1;
+}
+
+//initializes the wmc with settings as defined in wiiMoteCam.h
+uint8_t wmc_init() //FIXME: if wmc not connected, bus gets blocked (scl low) --> no eeprom comm
+{
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x30, 0x01)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x00,0x02)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x01,0x00)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x02,0x01)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x03,0x71)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x04,0x01)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x05,0x00)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x06,WMC_MAXSIZE)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x07,0x00)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x08,WMC_GAIN)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x1A,WMC_GAINLIMIT)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x1A,WMC_MINSIZE)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x33,0x03)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, 0x30,0x08)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+
+	DEBUG_PRINT("I2C connection [OK].\n");
+	return 1;
+}
+
+//reads all four blobs from the wmc, isVisible flag is updated every time, x y s x_angle and y_angle only when blob is visible
+uint8_t wmc_readBlobs(struct WmcBlob (*WMCBlob)[4])
+{
+	if(!i2cdevWriteByte(I2C1_DEV, WMC_ADR, I2CDEV_NO_MEM_ADDR, 0x37)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+
+	uint8_t buf[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+	if(!i2cdevRead(I2C1_DEV, WMC_ADR, I2CDEV_NO_MEM_ADDR, 12, buf)) { DEBUG_PRINT("I2C connection [FAIL].\n"); return 0; }
+
+	struct WmcBlob wmcBlobs[4];
+
+	wmcBlobs[0].x=buf[0]+((buf[2] & 0x30)<<4);
+	wmcBlobs[0].y=buf[1]+((buf[2] & 0xC0) <<2);
+	wmcBlobs[0].s=(buf[2] & 0x0F);
+
+	wmcBlobs[1].x=buf[3]+((buf[5] & 0x30) <<4);
+	wmcBlobs[1].y=buf[4]+((buf[5] & 0xC0) <<2);
+	wmcBlobs[1].s=(buf[5] & 0x0F);
+
+	wmcBlobs[2].x=buf[6]+((buf[8] & 0x30) <<4);
+	wmcBlobs[2].y=buf[7]+((buf[8] & 0xC0) <<2);
+	wmcBlobs[2].s=(buf[8] & 0x0F);
+
+	wmcBlobs[3].x=buf[9]+((buf[11] & 0x30) <<4);
+	wmcBlobs[3].y=buf[10]+((buf[11] & 0xC0) <<2);
+	wmcBlobs[3].s=(buf[11] & 0x0F);
+
+	uint8_t i;
+	for(i=0; i<4; i++)
+	{
+		if(wmcBlobs[i].y <= 767) //blob is visible/valid
+		{
+			(*WMCBlob)[i].isVisible = 1;
+			(*WMCBlob)[i].s = wmcBlobs[i].s;
+			(*WMCBlob)[i].x = wmcBlobs[i].x;
+			(*WMCBlob)[i].y = wmcBlobs[i].y;
+			(*WMCBlob)[i].x_angle = ((float)wmcBlobs[i].x - ((float)WMC_RES_X - 1) / 2) * (float)WMC_X_TO_ANGLE_FACTOR;
+			(*WMCBlob)[i].y_angle = ((float)wmcBlobs[i].y - ((float)WMC_RES_Y - 1) / 2) * (float)WMC_Y_TO_ANGLE_FACTOR;
+		}
+		else (*WMCBlob)[i].isVisible = 0;
+	}
+
+	return 1;
+}
---
+++ src/modules/interface/positionControl.h
@@ -0,0 +1,84 @@
+/*
+ * wmcPosition.h
+ *
+ *  Created on: 01.05.2015
+ *      Author: daniel
+ */
+
+#ifndef MODULES_INTERFACE_POSITIONCONTROL_H_
+#define MODULES_INTERFACE_POSITIONCONTROL_H_
+
+
+#include <stdint.h>
+
+//physical pattern layout distances in mm
+#define PATTERN_DISTANCE_L_R 90
+#define PATTERN_DISTANCE_M_F 60
+#define PATTERN_DISTANCE_L_M (PATTERN_DISTANCE_L_R / 2)
+#define PATTERN_DISTANCE_M_R (PATTERN_DISTANCE_L_R / 2)
+#define PATTERN_DISTANCE_L_F (hypotf(PATTERN_DISTANCE_L_M,PATTERN_DISTANCE_M_F))
+#define PATTERN_DISTANCE_R_F (hypotf(PATTERN_DISTANCE_M_R,PATTERN_DISTANCE_M_F))
+
+//calibration angles of wmc in rad(to account for non-perfect mounting)
+#define WMC_CAL_X 0
+#define WMC_CAL_Y 0
+
+//threshold for pattern allocation verification
+#define WMC_PATTERN_CORRECT_THRESHOLD 0.1
+
+//wmc status
+#define WMC_STATUS_OK 0
+#define WMC_STATUS_BLOBCOUNT_LOW_ERROR 1
+#define WMC_STATUS_BLOBCOUNT_HIGH_ERROR 2
+#define WMC_STATUS_PATTERN_ERROR 3
+
+//posCtrl modes
+#define POSCTRL_MODE_PATTERN 0
+#define POSCTRL_MODE_POINT 1
+
+#define OUT_OF_VIEW_TIMEOUT 2.0 //time in s after which posCtrl mode gets switched off if wmc status is not WMC_STATUS_OK, 0.0 == disabled
+
+//position smoothing factors, higher=more smoothing, slower response, 0 = no smoothing, 0.999999 = max TODO: optimize
+#define POS_SMOOTHING_ALT 0.3
+#define POS_SMOOTHING_YAW 0.1
+#define POS_SMOOTHING_X 0.7
+#define POS_SMOOTHING_Y 0.7
+
+//PID factors TODO: optimize
+#define PID_ALT_P 70.0
+#define PID_ALT_I 10.0
+#define PID_ALT_D 30.0
+#define PID_YAW_P 5.0
+#define PID_YAW_I 0.0
+#define PID_YAW_D 0.0
+#define PID_X_P 0.04
+#define PID_X_I 0.01 //vielZuKlein
+#define PID_X_D 0.03 //ZuGroß?
+#define PID_Y_P PID_X_P
+#define PID_Y_I PID_X_I
+#define PID_Y_D PID_X_D
+//alt PID integral limits TODO: optimize
+#define PID_ALT_INTEGRATION_LIMIT_HIGH 200000
+#define PID_ALT_INTEGRATION_LIMIT_LOW -200000
+
+//thrust needed to hover TODO: optimize, make dependend on battery voltage?
+#define THRUST_HOVER 40000
+
+//min/max values for rpyt TODO: optimize
+#define THRUST_MIN 10000
+#define THRUST_MAX 60000
+#define YAWRATE_MIN -400
+#define YAWRATE_MAX 400
+#define PITCH_MIN -10
+#define PITCH_MAX 10
+#define ROLL_MIN -10
+#define ROLL_MAX 10
+
+
+uint8_t positionControl_init(); //initializes positionControl
+uint8_t positionControl_update(); //updates positionControl, has to get called at IMU_UPDATE_FREQ Hz
+uint8_t positionControl_getRPYT(float *roll, float *pitch, float *yawRate, uint16_t *thrust); //saves desired control values to given pointers
+uint8_t positionControl_getWmcStatus();
+
+
+#endif /* MODULES_INTERFACE_POSITIONCONTROL_H_ */
